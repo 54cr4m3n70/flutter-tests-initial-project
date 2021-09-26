ab71b89aa292e89652c89c672253fc62d74ad1e2
Edilson Jardim Filho <edilsonj.filho@gmail.com>
2021-09-25 23:02:17 -0300

    Utilizando o predicate no teste
    Ajuste o teste para que, em vez de fazer a busca do ícone e texto, busque diretamente o FeatureItem com o ícone e texto esperado. Para isso, utilize o find.byWidgetPredicate().
    
    Em seguida, crie um teste para validar a existência da funcionalidade que lista as transferências.
    
    Nesse segundo teste, reutilize o mesmo código usado no teste que valida a funcionalidade de transferência. Para isso, extraia a lógica do predicate e reutilize no novo teste.
    
    Rode ambos os testes e verifique se passa conforme o esperado.
    
    by: Edilson Jardim Filho <edilsonj.filho@gmail.com>

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -26,15 +26,41 @@ void main() {
           home: Dashboard(),
         ),
       );
-      final iconTransferFeatureItem =
-          find.widgetWithIcon(FeatureItem, Icons.monetization_on);
 
-      expect(iconTransferFeatureItem, findsWidgets);
-
-      final nameTransferFeatureItem =
-          find.widgetWithText(FeatureItem, 'Transfer');
-
-      expect(nameTransferFeatureItem, findsOneWidget);
+      final transferFeatureItem = find.byWidgetPredicate(
+        (widget) => featureItemMatcher(
+          widget,
+          'Transfer',
+          Icons.monetization_on,
+        ),
+      );
+      expect(transferFeatureItem, findsOneWidget);
     },
   );
+
+  testWidgets(
+    'Should display the transaction feed feature when the Dashboard is opened',
+    (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Dashboard(),
+        ),
+      );
+      final transactionFeedFeatureItem = find.byWidgetPredicate(
+        (widget) => featureItemMatcher(
+          widget,
+          'Transaction Feed',
+          Icons.description,
+        ),
+      );
+      expect(transactionFeedFeatureItem, findsOneWidget);
+    },
+  );
+}
+
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if (widget is FeatureItem) {
+    return widget.name == name && widget.icon == icon;
+  }
+  return false;
 }
